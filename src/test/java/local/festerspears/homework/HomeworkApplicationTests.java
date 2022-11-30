aae13414ac48e39ab29b69cbc79e31e147e38ae7
Eric Reichert <eric.reichert@ntrepidcorp.com>
2022-11-30 08:40:12 -0500

    initial commit

---
+++ src/test/java/local/festerspears/homework/HomeworkApplicationTests.java
@@ -0,0 +1,16 @@
+package local.festerspears.homework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(false, true);
+	}
+
+}
